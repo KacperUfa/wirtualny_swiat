8e3b111a1cbb3446e4bef958ac504eafb29187a0
KacperUfa <kacperufa@gmail.com>
2021-04-28 23:16:23 +0200

    adding 'zwierze' class

---
+++ src/Zwierze.h
@@ -0,0 +1,15 @@
+//
+// Created by kacpe on 28.04.2021.
+//
+
+#ifndef WIRTUALNY_SWIAT_ZWIERZE_H
+#define WIRTUALNY_SWIAT_ZWIgit ERZE_H
+#include "Organizm.h"
+
+class Zwierze : public Organizm {
+public:
+
+};
+
+
+#endif //WIRTUALNY_SWIAT_ZWIERZE_H
